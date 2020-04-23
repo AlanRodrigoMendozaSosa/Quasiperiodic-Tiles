a856f34f2d3fff4d27caa60a58ae2fdede9f5c6a
Alan Rodrigo Mendoza Sosa <alanmendoza@ciencias.unam.mx>
2020-04-23 04:10:21 -0500

    Add files via upload

--- Demo/Main_Cluster_Size/Algoritmo_Radio_Promedio_Cluster_Principal.jl
+++ Demo/Main_Cluster_Size/Algoritmo_Radio_Promedio_Cluster_Principal.jl
@@ -6,35 +6,35 @@
 #"Iteraciones_Algoritmo" es el número entero de puntos y sus vecindades del arreglo cuasiperiódico que consideraremos.
 function radio_Seguro(Margen_Error, Semilado, Iteraciones_Areas, Cota_Area, Iteraciones_Algoritmo, Promedios_Distancia, Vectores_Estrella, Arreglo_Alfas)
     Radio_Seguro = 0; #Definimos la variable que nos dará el radio del parche
-    Contador_Iteraciones = 0; #Contador de las iteraciones que se han realizado
 
     for i in 1:Iteraciones_Algoritmo
-        #Generación del arreglo cuasiperiódico con margen de error (1era entrada) y semilado del cuadrado donde 
-        #colocar un pto arbitrario.
-        Coordenadas_X, Coordenadas_Y, Punto = region_Local_Voronoi(Margen_Error, Semilado, Promedios_Distancia, Vectores_Estrella, Arreglo_Alfas);
+        #Generamos un punto arbitrario en un cuadrado centrado en el origen de semilado "Semilado"
+        Punto_Arbitrario = punto_Arbitrario(Semilado);
+
+        #Generación del arreglo cuasiperiódico con margen de error (1era entrada)
+        Coordenadas_Vertices = region_Local_Voronoi(Margen_Error, Promedios_Distancia, Vectores_Estrella, Arreglo_Alfas, Punto_Arbitrario);
 
         #Obtención de los centroides de los poligonos del arreglo cuasiperiódico en: 
         #coordenada X, coordenada Y, Float64(X,Y) y su diccionario Centroides <-> Vertices.
-        Centroides_X, Centroides_Y, Centroides, Diccionario_Centroides = centroides(Coordenadas_X, Coordenadas_Y);
+        Centroides, Diccionario_Centroides = centroides(Coordenadas_Vertices);
         
         #Obtención de los centroides de los buenos polígonos tras iterar N veces (3era entrada) algoritmo de áreas.
         Centroides_Iterados = centroides_Area_Acotada_Iterada(Centroides, Cota_Area, Iteraciones_Areas);
         
         #Visualizamos ahora el arreglo cuasiperiódico tras quitarle las n capas.
-        Coordenadas_X_2, Coordenadas_Y_2 = centroides_A_Vertices(Centroides_Iterados, Diccionario_Centroides);
+        Coordenadas_X, Coordenadas_Y = centroides_A_Vertices(Centroides_Iterados, Diccionario_Centroides);
         
         #Algoritmo para encontrar el radio seguro de los polígonos de Voronoi en un arreglo cuadrado.
-        MaximoX = maximum(Coordenadas_X_2)
-        MaximoY = maximum(Coordenadas_Y_2)
-        MinimoX = minimum(Coordenadas_X_2)
-        MinimoY = minimum(Coordenadas_Y_2)
+        MaximoX = maximum(Coordenadas_X)
+        MaximoY = maximum(Coordenadas_Y)
+        MinimoX = minimum(Coordenadas_X)
+        MinimoY = minimum(Coordenadas_Y)
 
-        Distancias = [abs(MaximoX-Punto[1]), abs(MinimoX-Punto[1]), abs(MaximoY-Punto[2]), abs(MinimoY-Punto[2])]
+        Distancias = [abs(MaximoX-Punto_Arbitrario[1]), abs(MinimoX-Punto_Arbitrario[1]), abs(MaximoY-Punto_Arbitrario[2]), abs(MinimoY-Punto_Arbitrario[2])]
 
         Radio_Seguro += minimum(Distancias)
         
-        Contador_Iteraciones += 1;
-        println("Se han realizado ", Contador_Iteraciones, " iteraciones.")
+        println("Se han realizado $(i) iteraciones.")
     end
     
     return Radio_Seguro/Iteraciones_Algoritmo
--- Demo/Main_Cluster_Size/Generador_Vecindades_Pto_Arbitrario.jl
+++ Demo/Main_Cluster_Size/Generador_Vecindades_Pto_Arbitrario.jl
@@ -1,14 +1,14 @@
-#Función que dado un Punto, obtiene aproximadamente los enteros que forman el polígono que lo contiene
-#"Punto" es un arreglo dos dimensional con las coordenadas de un punto en el espacio 2D
-#"Arreglo_Distancias_Promedio_Rectas_V" es un arreglo con la separación promedio entre rectas ortogonales a cada vector estrella
-#"Vectores_Estrella" es el conjunto de vectores estrella
-function proyecciones_Pto_Direccion_Franjas(Punto, Arreglo_Distancias_Promedio_Rectas_V, Vectores_Estrella)
-
-    #Obtengamos ahora la proyección del punto con la línea de cada una de las franjas generadas por cada vector
+#Función que dado un Punto, obtiene aproximadamente los enteros que forman el polígono que lo contiene.
+#"Punto" es un arreglo [X,Y] con las coordenadas de un punto en el espacio 2D.
+#"Promedios_Distancia" es el arreglo con la separación entre las franjas cuasiperiódicas.
+#"Vectores_Estrella" es el arreglo con los vectores estrella que generan la retícula cuasiperiódica deseada.
+function proyecciones_Pto_Direccion_Franjas(Punto, Promedios_Distancia, Vectores_Estrella)
+    #Paso 1: Generemos un arreglo en donde irán los números reales resultado de proyectar el Punto con los vectores estrella.
     Arreglo_Proyeccion_Pto_Direccion_Franjas = [];
     
+    #Paso 2: Para cada vector estrella, proyectamos el Punto sobre dicho vector y reescalamos con la separación entre las franjas cuasiperiódicas.
     for i in 1:length(Vectores_Estrella)
-        Proyeccion = prod_Punto(Punto, Vectores_Estrella[i])/Arreglo_Distancias_Promedio_Rectas_V[i];
+        Proyeccion = prod_Punto(Punto, Vectores_Estrella[i])/Promedios_Distancia[i];
         #Al parecer no hay que restarle "- Arreglo_Distancia_Origen_Primera_Recta[i]" que es la DOPR
         push!(Arreglo_Proyeccion_Pto_Direccion_Franjas, Proyeccion);
     end
@@ -16,30 +16,30 @@ function proyecciones_Pto_Direccion_Franjas(Punto, Arreglo_Distancias_Promedio_R
     return Arreglo_Proyeccion_Pto_Direccion_Franjas
 end
 
-#Función que genera los vértices de un arreglo cuasiperiódico asociados a la vecindad de un Punto arbitrario
-#"Proyecciones" es el conjunto de números enteros candidatos a ser los que generan las rectas ortogonales a los vectores Ej y Ek que contienen al punto
-#"Vectores_Estrella" es el conjunto de vectores estrella
-#"Arreglo_Alfas" es el conjunto con las constantes alfas asociadas a cada vector estrella
-#El parámetro "N" nos sirve para "corregir" el posible error numérico al determinar el número entero asociado a cada vector
+#Función que genera los vértices de un arreglo cuasiperiódico asociados a la vecindad de un Punto arbitrario.
+#"Proyecciones" es el conjunto de números enteros candidatos a ser los que generan el polígono que contiene al punto.
+#"Vectores_Estrella" es el conjunto de vectores estrella.
+#"Arreglo_Alfas" es el arreglo con los valores numéricos de la separación respecto al origen del conjunto de rectas ortogonales a los vectores estrella en el método generalizado dual.
+#"N" es el margen de error asociado a los números enteros generados por la proyección del punto sobre los vectores estrella.
 function generador_Vecindades_Vertices(Proyecciones, Vectores_Estrella, Arreglo_Alfas, N)
-    #Definimos el arreglo que contendra los vértices asociados a cada combinación
+    #Paso 1: Definimos el arreglo que contendrá a los vértices asociados a cada combinación de vectores estrella (incluídos los generados al considerar el margen de error)
     Puntos_Red_Dual = [];
-    Informacion_Puntos_Red_Dual = [];
     
+    #Paso 2: Consideramos todas las posibles combinaciones de vectores estrella con los posibles números enteros correspondientes (y su margen de error)
     for i in 1:length(Vectores_Estrella)
         for j in i+1:length(Vectores_Estrella)
-            
+            #Consideramos el margen de error a cada número entero
             for n in -N:N
                 for m in -N:N
-                    try #Vamos a dejar que el try ... catch se encargue de los casos en que los vectores estrella sean paralelos
-                        #println(i,j)
-                        #Obtengamos los vértices del arreglo considerando los vectores Ei y Ej con sus respectivos números enteros
-                        t0, t1, t2, t3, info = cuatro_Regiones(i, j, round(Proyecciones[i])+n, round(Proyecciones[j])+m, Vectores_Estrella, Arreglo_Alfas);
+                    #Vamos a dejar que el try ... catch se encargue de los casos en que los vectores estrella sean paralelos
+                    try
+                        #Obtengamos los vértices del arreglo considerando los vectores Ei y Ej con sus respectivos números enteros (y su margen de error)
+                        #Salida: [X,Y]
+                        t0, t1, t2, t3 = cuatro_Regiones(i, j, round(Proyecciones[i])+n, round(Proyecciones[j])+m, Vectores_Estrella, Arreglo_Alfas);
                         push!(Puntos_Red_Dual, t0);
                         push!(Puntos_Red_Dual, t1);
                         push!(Puntos_Red_Dual, t2);
                         push!(Puntos_Red_Dual, t3);
-                        push!(Informacion_Puntos_Red_Dual, info);
                     catch
                         nothing;
                     end
@@ -49,5 +49,5 @@ function generador_Vecindades_Vertices(Proyecciones, Vectores_Estrella, Arreglo_
         end
     end
     
-    return Puntos_Red_Dual, Informacion_Puntos_Red_Dual
+    return Puntos_Red_Dual
 end
