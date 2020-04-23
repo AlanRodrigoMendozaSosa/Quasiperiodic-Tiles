f342a8d04577b0157280b03623e8d0b6b5da357c
Alan Rodrigo Mendoza Sosa <alanmendoza@ciencias.unam.mx>
2020-04-23 04:09:44 -0500

    Add files via upload

--- Demo/Number_of_Edges_Distribution_Main_Cluster_(Voronoi's_Polygons)/Algoritmo_Numero_Lados_Poligonos_Voronoi.jl
+++ Demo/Number_of_Edges_Distribution_Main_Cluster_(Voronoi's_Polygons)/Algoritmo_Numero_Lados_Poligonos_Voronoi.jl
@@ -6,21 +6,40 @@
 #"Margen_Error" es el parámetro que determina qué margen de error se considera al generar los posibles números enteros asociados al polígono de Voronoi que contiene 
 #al punto arbitrario.
 #"Semilado_Caja" es el semilado de un cuadrado en el cual se genera un punto arbitrario, alrededor del cual se genera la vecindad del arreglo cuasiperiódico.
+#"Promedios_Distancia" es el arreglo con la separación entre las franjas cuasiperiódicas.
+#"Vectores_Estrella" es el arreglo con los vectores estrella que generan la retícula cuasiperiódica deseada.
+#"Arreglo_Alfas" es el arreglo con los valores numéricos de la separación respecto al origen del conjunto de rectas ortogonales a los vectores estrella en el método generalizado dual.
 function arreglo_Numero_Lados_Buenos_Poligonos(Iteraciones, Iteraciones_Cluster, Area_Cota, Margen_Error, Semilado_Caja, Promedios_Distancia, Vectores_Estrella, Arreglo_Alfas)
-    Arreglo_Numero_Lados_Total = []; #Arreglo donde irán el número de lados de los polígonos tras todas las iteraciones
-    Contador_Iteraciones = 0; #Contador de las iteraciones que se han realizado
+    Arreglo_Numero_Lados_Total = []; #Arreglo donde irá el número de lados de los polígonos tras todas las iteraciones
     Arreglo_Arreglos_Vertices = []; #Arreglo que contendrá los arreglos con los vértices de cada polígono
     
     for i in 1:Iteraciones
         #Arreglo donde irán los vértices de los polígonos que viven en el cluster principal de cada una de las vecindades generadas
         Arreglo_Poligonos_Cluster_Principal = [];
 
+        #Generamos un punto arbitrario dentro de un cuadrado centrado en el origen de semilado "Semilado_Caja"
+        APoint = Float64[]; #Arreglo donde colocaremos las coordenadas del punto arbitrario generado
+
+        #Generamos dos parámetros que determinarán donde se localiza el punto arbitrario dentro de la caja
+        x = rand();
+        y = rand();
+
+        if (x > 0.5) && (y > 0.5)
+            APoint = [rand()*Semilado_Caja, rand()*Semilado_Caja];
+        elseif (x > 0.5) && (y < 0.5)
+            APoint = [rand()*Semilado_Caja, -rand()*Semilado_Caja];
+        elseif (x < 0.5) && (y > 0.5)
+            APoint = [-rand()*Semilado_Caja, rand()*Semilado_Caja];
+        elseif (x < 0.5) && (y < 0.5)
+            APoint = [-rand()*Semilado_Caja, -rand()*Semilado_Caja];
+        end
+
         #Generación del arreglo cuasiperiódico con margen de error (1era entrada) y semilado del cuadrado donde colocar un pto arbitrario.
-        Coordenadas_X, Coordenadas_Y, Punto = region_Local_Voronoi(Margen_Error, Semilado_Caja, Promedios_Distancia, Vectores_Estrella, Arreglo_Alfas);
+        Puntos_Duales = region_Local_Voronoi(Margen_Error, Promedios_Distancia, Vectores_Estrella, Arreglo_Alfas, APoint);
 
         #Obtención de los centroides de los poligonos del arreglo cuasiperiódico en: 
         #coordenada X, coordenada Y, Float64(X,Y) y su diccionario Centroides <-> Vertices.
-        Centroides_X, Centroides_Y, Centroides, Diccionario_Centroides = centroides(Coordenadas_X, Coordenadas_Y);
+        Centroides, Diccionario_Centroides = centroides(Puntos_Duales);
         
         #Nos quedamos únicamente con los centroides correspondientes a los polígonos del cluster principal
         Centroides_Iterados = centroides_Area_Acotada_Iterada(Centroides, Area_Cota, Iteraciones_Cluster);
@@ -34,8 +53,8 @@ function arreglo_Numero_Lados_Buenos_Poligonos(Iteraciones, Iteraciones_Cluster,
         #Obtenemos el diccionario "Centroides -> Indices Poligonos Voronoi" de la configuración inicial
         Diccionario_Centroides_Indice = diccionario_Centroides_Indice_Voronoi(sites_Inicial, voronoi_Inicial)
 
-        for i in Centroides_Iterados #Checamos únicamente los polígonos del cluster principal
-            Face = voronoi_Inicial.faces[Diccionario_Centroides_Indice[i]]; #Polígono del clúster principal que estamos evaluando
+        for j in Centroides_Iterados #Checamos únicamente los polígonos del cluster principal
+            Face = voronoi_Inicial.faces[Diccionario_Centroides_Indice[j]]; #Polígono del clúster principal que estamos evaluando
             Arreglo_Coordenadas_Vertices = []; #Arreglo donde iremos poniendo las coordenadas de los vértices de los pol. Voronoi
             Halfedge = Face.outerComponent; #Elemento de los pol. Voronoi, si es igual a "nothing" el pol. no se cierra
             Vertice_Inicial = Halfedge.origin.coordinates; #Coordenadas del primer vértice
@@ -60,8 +79,7 @@ function arreglo_Numero_Lados_Buenos_Poligonos(Iteraciones, Iteraciones_Cluster,
         #esta iteración
         push!(Arreglo_Arreglos_Vertices, Arreglo_Poligonos_Cluster_Principal);
         
-        Contador_Iteraciones += 1;
-        println("Se han realizado ", Contador_Iteraciones, " iteraciones.")
+        println("Se han realizado $(i) iteraciones.")
     end
 
     return Arreglo_Numero_Lados_Total, Arreglo_Arreglos_Vertices
