2ee0eb31978e9e69360528ee15d7d5faf290a2e5
Alan Rodrigo Mendoza Sosa <alanmendoza@ciencias.unam.mx>
2020-04-23 04:06:18 -0500

    Add files via upload

--- Demo/Container_Polygon_(Voronoi's_Algorithm)/Operaciones_Basicas.jl
+++ Demo/Container_Polygon_(Voronoi's_Algorithm)/Operaciones_Basicas.jl
@@ -9,3 +9,25 @@ end
 function vector_Ortogonal(A)
     return [A[2], -A[1]]
 end
+
+#Función que nos genera un punto aleatorio en un cuadrado de semilado SL centrado en el origen.
+function punto_Arbitrario(SL)
+    #Definimos la variable Punto que contendrá las coordenadas del punto de interés
+    Punto = zeros(Float64,2);
+
+    #Llaves para determinar el cuadrante donde estará el punto
+    x = rand();
+    y = rand();
+
+    if (x > 0.5) && (y > 0.5)
+        Punto = [rand()*SL, rand()*SL];
+    elseif (x > 0.5) && (y < 0.5)
+        Punto = [rand()*SL, -rand()*SL];
+    elseif (x < 0.5) && (y > 0.5)
+        Punto = [-rand()*SL, rand()*SL];
+    elseif (x < 0.5) && (y < 0.5)
+        Punto = [-rand()*SL, -rand()*SL];
+    end
+
+    return Punto
+end
