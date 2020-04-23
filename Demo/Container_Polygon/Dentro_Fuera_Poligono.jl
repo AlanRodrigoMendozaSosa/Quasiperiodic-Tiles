479699ce30b788c929c4937d500e8bfc4efd7ab8
Alan Rodrigo Mendoza Sosa <alanmendoza@ciencias.unam.mx>
2020-04-23 04:05:19 -0500

    Add files via upload

--- Demo/Container_Polygon/Dentro_Fuera_Poligono.jl
+++ Demo/Container_Polygon/Dentro_Fuera_Poligono.jl
@@ -69,7 +69,7 @@ end
 #"Punto" es el punto que queremos checar si está dentro o fuera. Es un arreglo con dos entradas.
 function dentro(Poligono, Punto)
     #Generamos una recta con dirección "arbitraria".
-    Recta = segmento(Punto, [100*cos(rand(0:1e-6:2*pi)),100*sin(rand(0:1e-6:2*pi))]);
+    Recta = segmento(Punto, [1e6*cos(rand(0:1e-6:2*pi)),1e6*sin(rand(0:1e-6:2*pi))]);
     
     Contenedor = [];
     
@@ -80,7 +80,7 @@ function dentro(Poligono, Punto)
     end
     
     #Calculemos el número de intersecciones que hay entre los segmentos del arreglo prueba.
-    #Hay que regresar true o false, en lugar del texto, para que esto sea funcional.
+    #Si el número de intersecciones es cero, manda "false"
     if iseven(length(Contenedor))
         return false;
     else
@@ -96,14 +96,12 @@ function obtener_Segmentos_Vertices(Coordenadas_X, Coordenadas_Y)
     Poligonos = [];
         
     for i in 1:4:length(Coordenadas_X)
-        
         Segmento1 = segmento([Coordenadas_X[i], Coordenadas_Y[i]], [Coordenadas_X[i+1], Coordenadas_Y[i+1]]);
         Segmento2 = segmento([Coordenadas_X[i+1], Coordenadas_Y[i+1]], [Coordenadas_X[i+2], Coordenadas_Y[i+2]]);
         Segmento3 = segmento([Coordenadas_X[i+2], Coordenadas_Y[i+2]], [Coordenadas_X[i+3], Coordenadas_Y[i+3]]);
         Segmento4 = segmento([Coordenadas_X[i+3], Coordenadas_Y[i+3]], [Coordenadas_X[i], Coordenadas_Y[i]]);
         
         push!(Poligonos, [Segmento1, Segmento2, Segmento3, Segmento4]);
-        
     end
     
     return Poligonos
@@ -113,14 +111,18 @@ end
 #"Punto" es el punto que queremos checar si está dentro o fuera. Es un arreglo con dos entradas.
 #"Poligonos" es un arreglo de arreglos, cada uno con los segmentos que conforman al polígono en cuestión.
 #"Informacion_Duales" es un arreglo con la información de cómo se generó cada polígono.
-function encontrar_Poligono(Punto, Poligonos, Informacion_Duales)
+function encontrar_Poligono(Punto, Poligonos)
+    #Definimos un contenedor donde colocaremos las coordenadas [X,Y] de los vértices del polígono contenedor
+    Vertices = [];
     #Iteremos sobre los posibles polígonos para encontrar el que contenga al punto
     for i in 1:length(Poligonos)
-        #Si el polígono i-ésimo contiene al punto, regresa la info de ese polígono
+        #Si el polígono i-ésimo contiene al punto, regresa los segmentos que lo conforman
         if dentro(Poligonos[i], Punto)
-            return Informacion_Duales[i]
+            for j in 1:4
+                push!(Vertices, Poligonos[i][j].inicio);
+            end
+            return Vertices
         end
-        
     end
     #Si no encuentra polígono, que nos mande impresión con dicha información
     println("Error: No hay polígono que contenga al punto")
