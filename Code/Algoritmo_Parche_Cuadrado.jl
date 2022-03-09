cedd9f8d9d482f9adaf3f1fac5342f02b80dfb2b
Alan Rodrigo Mendoza Sosa <alanmendoza@ciencias.unam.mx>
2022-03-09 17:53:35 -0600

    Update algorithm to get the main cluster
    Files with the update of the new algorithm to obtain the main cluster of tiles of the quasiperiodic array around an arbitrary point in the plane. The previous version iterated an algorithm based on an upper bound for the area of ​​the Voronoi tiles associated with the centroids of the quasiperiodic tiles N number of times. The new algorithm uses the upper bound for the area of ​​the Voronoi tiles to identify the border tile closest to the center of the generated patch, then removes all tiles that are further from the center than the closest border tile.

--- Code/Algoritmo_Parche_Cuadrado.jl
+++ Code/Algoritmo_Parche_Cuadrado.jl
@@ -7,12 +7,12 @@
 #"Punto" es el punto alrededor de donde se va a generar la vecindad.
 function parche_Cuadrado(Informacion_Parche, Factor_Reduccion, Promedios_Distancia, Vectores_Estrella, Arreglo_Alfas, Punto)    
     #Paso 1: Generemos los cuatro centros para los parches circulares.
-    #Informacion_Parche[6] es el radio promedio del parche circular.
+    #Informacion_Parche[5] es el radio promedio del parche circular.
     #Salida: [X,Y]
-    P_1 = Punto + [(sqrt(2)/2)*(Factor_Reduccion)*(Informacion_Parche[6]), (sqrt(2)/2)*(Factor_Reduccion)*(Informacion_Parche[6])];
-    P_2 = Punto + [-(sqrt(2)/2)*(Factor_Reduccion)*(Informacion_Parche[6]), (sqrt(2)/2)*(Factor_Reduccion)*(Informacion_Parche[6])];
-    P_3 = Punto - [(sqrt(2)/2)*(Factor_Reduccion)*(Informacion_Parche[6]), (sqrt(2)/2)*(Factor_Reduccion)*(Informacion_Parche[6])];
-    P_4 = Punto + [(sqrt(2)/2)*(Factor_Reduccion)*(Informacion_Parche[6]), -(sqrt(2)/2)*(Factor_Reduccion)*(Informacion_Parche[6])];
+    P_1 = Punto + [(sqrt(2)/2)*(Factor_Reduccion)*(Informacion_Parche[5]), (sqrt(2)/2)*(Factor_Reduccion)*(Informacion_Parche[5])];
+    P_2 = Punto + [-(sqrt(2)/2)*(Factor_Reduccion)*(Informacion_Parche[5]), (sqrt(2)/2)*(Factor_Reduccion)*(Informacion_Parche[5])];
+    P_3 = Punto - [(sqrt(2)/2)*(Factor_Reduccion)*(Informacion_Parche[5]), (sqrt(2)/2)*(Factor_Reduccion)*(Informacion_Parche[5])];
+    P_4 = Punto + [(sqrt(2)/2)*(Factor_Reduccion)*(Informacion_Parche[5]), -(sqrt(2)/2)*(Factor_Reduccion)*(Informacion_Parche[5])];
     
     #Paso 2: Generamos las vecindades de los arreglos cuasiperiódicos centrados en esos puntos dados.
     #Informacion_Parche[1] es el margen de error a los números enteros "n_j" empleado al generar los parches circulares.
@@ -29,26 +29,35 @@ function parche_Cuadrado(Informacion_Parche, Factor_Reduccion, Promedios_Distanc
     Centroides2, Diccionario_Centroides2 = centroides(Coordenadas_2);
     Centroides3, Diccionario_Centroides3 = centroides(Coordenadas_3);
     Centroides4, Diccionario_Centroides4 = centroides(Coordenadas_4);
+
+    #Paso 4: Generamos los teselados de Voronoi asociados a los centroides.
+    sites1 = [(Centroides1[i][1], Centroides1[i][2]) for i in 1:length(Centroides1)]
+    sites2 = [(Centroides2[i][1], Centroides2[i][2]) for i in 1:length(Centroides2)]
+    sites3 = [(Centroides3[i][1], Centroides3[i][2]) for i in 1:length(Centroides3)]
+    sites4 = [(Centroides4[i][1], Centroides4[i][2]) for i in 1:length(Centroides4)]
+    Initial_Voronoi1 = getVoronoiDiagram(sites1);
+    Initial_Voronoi2 = getVoronoiDiagram(sites2);
+    Initial_Voronoi3 = getVoronoiDiagram(sites3);
+    Initial_Voronoi4 = getVoronoiDiagram(sites4);
     
-    #Paso 4: Depuramos los arreglos cuasiperiódicos obtenidos para quedarnos con el clúster principal.
-    #Informacion_Parche[4] es la cota del área empleada como discriminante en el algoritmo de Voronoi.
-    #Informacion_Parche[3] es el número de iteraciones del algoritmo de Voronoi para obtener el cluster principal.
+    #Paso 5: Depuramos los arreglos cuasiperiódicos obtenidos para quedarnos con el clúster principal.
+    #Informacion_Parche[3] es la cota del área empleada como discriminante en el algoritmo de Voronoi.
     #Salida: [Float64(X,Y)]
-    Centroides_Iterados1 = centroides_Area_Acotada_Iterada(Centroides1, Informacion_Parche[4], Informacion_Parche[3]);
-    Centroides_Iterados2 = centroides_Area_Acotada_Iterada(Centroides2, Informacion_Parche[4], Informacion_Parche[3]);
-    Centroides_Iterados3 = centroides_Area_Acotada_Iterada(Centroides3, Informacion_Parche[4], Informacion_Parche[3]);
-    Centroides_Iterados4 = centroides_Area_Acotada_Iterada(Centroides4, Informacion_Parche[4], Informacion_Parche[3]);
+    Centroides_Principal1 = centroides_Area_Acotada(Initial_Voronoi1, Informacion_Parche[3], P_1);
+    Centroides_Principal2 = centroides_Area_Acotada(Initial_Voronoi2, Informacion_Parche[3], P_2);
+    Centroides_Principal3 = centroides_Area_Acotada(Initial_Voronoi3, Informacion_Parche[3], P_3);
+    Centroides_Principal4 = centroides_Area_Acotada(Initial_Voronoi4, Informacion_Parche[3], P_4);
     
-    #Paso 5: Agrupamos todos los buenos centroides en un único arreglo, quedándonos únicamente con los centroides sin repetir.
-    Arreglo_Centroides = vcat(Centroides_Iterados1, Centroides_Iterados2, Centroides_Iterados3, Centroides_Iterados4);
+    #Paso 6: Agrupamos todos los buenos centroides en un único arreglo, quedándonos únicamente con los centroides sin repetir.
+    Arreglo_Centroides = vcat(Centroides_Principal1, Centroides_Principal2, Centroides_Principal3, Centroides_Principal4);
     unique!(Arreglo_Centroides);
     #println("There is a total of $(length(Arreglo_Centroides)) polygons with possible duplicates.")
     #println("There is a total of $(length(Arreglo_Centroides)) uniques polygons.")
     
-    #Paso 6: Generamos un único diccionario al unir los 4 diccionarios generados previamente
+    #Paso 7: Generamos un único diccionario al unir los 4 diccionarios generados previamente
     Diccionario_Centroides_Unico = merge(Diccionario_Centroides1, Diccionario_Centroides2, Diccionario_Centroides3, Diccionario_Centroides4)
 
-    #Paso 7: Recuperamos los polígonos del arrego cuasiperiódico que corresponden a los clúster principales.
+    #Paso 8: Recuperamos los polígonos del arreglo cuasiperiódico que corresponden a los clúster principales.
     Coordenadas_X_CP, Coordenadas_Y_CP = centroides_A_Vertices(Arreglo_Centroides, Diccionario_Centroides_Unico);
     
     return Coordenadas_X_CP, Coordenadas_Y_CP
