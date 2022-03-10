46c73d04555399e1953a5b7110ae4b98963fe852
Alan Rodrigo Mendoza Sosa <alanmendoza@ciencias.unam.mx>
2022-03-09 18:46:22 -0600

    Update algorithm to get the main cluster

--- Code/Algoritmo_Radio_Promedio_Cluster_Principal.jl
+++ Code/Algoritmo_Radio_Promedio_Cluster_Principal.jl
@@ -1,10 +1,9 @@
 #Función que calcula el radio promedio del cluster principal de diferentes configuraciones.
 #"Margen_Error" es el número entero que consideramos como error para las proyecciones del punto de interés con los vectores estrella.
 #"Semilado" es el número flotante asociado al semilado de un cuadrado centrado en el origen dentro del cual pondremos un punto aleatorio.
-#"Iteraciones_Areas" es el número entero de veces que aplicaremos el algoritmo de quitar polígonos por el discriminante del área a cada arreglo de centroides.
 #"Cota_Area" es un número flotante que sirve como área discrminante de polígonos del cluster principal de los que no.
 #"Iteraciones_Algoritmo" es el número entero de puntos y sus vecindades del arreglo cuasiperiódico que consideraremos.
-function radio_Seguro(Margen_Error, Semilado, Iteraciones_Areas, Cota_Area, Iteraciones_Algoritmo, Promedios_Distancia, Vectores_Estrella, Arreglo_Alfas)
+function radio_Seguro(Margen_Error, Semilado, Cota_Area, Iteraciones_Algoritmo, Promedios_Distancia, Vectores_Estrella, Arreglo_Alfas)
     Radio_Seguro = 0; #Definimos la variable que nos dará el radio del parche
 
     for i in 1:Iteraciones_Algoritmo
@@ -17,12 +16,16 @@ function radio_Seguro(Margen_Error, Semilado, Iteraciones_Areas, Cota_Area, Iter
         #Obtención de los centroides de los poligonos del arreglo cuasiperiódico en: 
         #coordenada X, coordenada Y, Float64(X,Y) y su diccionario Centroides <-> Vertices.
         Centroides, Diccionario_Centroides = centroides(Coordenadas_Vertices);
+
+        #Obtención del teselado de Voronoi a partir de los Centroides
+        sites = [(Centroides[i][1], Centroides[i][2]) for i in 1:length(Centroides)]
+        Initial_Voronoi = getVoronoiDiagram(sites);
         
         #Obtención de los centroides de los buenos polígonos tras iterar N veces (3era entrada) algoritmo de áreas.
-        Centroides_Iterados = centroides_Area_Acotada_Iterada(Centroides, Cota_Area, Iteraciones_Areas);
+        Centroides_Principal = centroides_Area_Acotada(Initial_Voronoi, Cota_Area, Punto_Arbitrario);
         
         #Visualizamos ahora el arreglo cuasiperiódico tras quitarle las n capas.
-        Coordenadas_X, Coordenadas_Y = centroides_A_Vertices(Centroides_Iterados, Diccionario_Centroides);
+        Coordenadas_X, Coordenadas_Y = centroides_A_Vertices(Centroides_Principal, Diccionario_Centroides);
         
         #Algoritmo para encontrar el radio seguro de los polígonos de Voronoi en un arreglo cuadrado.
         MaximoX = maximum(Coordenadas_X)
