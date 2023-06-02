55863a6c07da6108d1d2c80d7363402f4069065f
Alan Rodrigo Mendoza Sosa <alanmendoza@ciencias.unam.mx>
2023-06-02 17:33:19 -0600

    Delete Hyperuniformity Simulation directory

--- Hyperuniformity Simulation/abstractTypes.jl
+++
@@ -1,8 +0,0 @@
-abstract type AbstractVoronoiNode end
-
-abstract type AbstractVoronoiEvent end
-
-abstract type AbstractPolygonalMesh end
-abstract type AbstractVertex <: AbstractPolygonalMesh end
-abstract type AbstractFace <: AbstractPolygonalMesh end
-abstract type AbstractHalfedge <: AbstractPolygonalMesh end
